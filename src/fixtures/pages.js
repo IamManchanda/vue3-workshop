3c94154cd7ebe1f737076155dd6315a58e1182a3
Harry Manchanda <harmanmanchanda182@gmail.com>
2020-09-24 20:39:33 +0530

    Code refactor - pages fixtures

---
+++ src/fixtures/pages.js
@@ -0,0 +1,44 @@
+import { v4 as uuidv4 } from "uuid";
+
+export default [
+  {
+    id: uuidv4(),
+    path: "/counter",
+    title: "Counter (Non Workshop)",
+  },
+  {
+    id: uuidv4(),
+    path: "/list-of-items",
+    title: "List of items",
+  },
+  {
+    id: uuidv4(),
+    path: "/checked-names",
+    title: "Checked Names",
+  },
+  {
+    id: uuidv4(),
+    path: "/do-you-like-tacos",
+    title: "Do you like tacos? (radio example)",
+  },
+  {
+    id: uuidv4(),
+    path: "/iterate-an-object",
+    title: "Iterate an object",
+  },
+  {
+    id: uuidv4(),
+    path: "favorite-kind-of-taco",
+    title: "Favorite kind of taco? (v-pre and v-once)",
+  },
+  {
+    id: uuidv4(),
+    path: "backpack-shop-counter",
+    title: "Backpack Shop Counter",
+  },
+  {
+    id: uuidv4(),
+    path: "cross-site-scripting",
+    title: "Cross Site Scripting - XSS Software Attack (v-html)",
+  },
+];
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -14,7 +14,8 @@
 <script>
 //#region Imports
 import { reactive, toRefs } from "vue";
-import { v4 as uuidv4 } from "uuid";
+
+import pagesFixture from "../fixtures/pages.js";
 //#endregion
 
 export default {
@@ -22,48 +23,7 @@ export default {
   setup() {
     //#region Reactive References
     const state = reactive({
-      pages: [
-        {
-          id: uuidv4(),
-          path: "/counter",
-          title: "Counter (Non Workshop)",
-        },
-        {
-          id: uuidv4(),
-          path: "/list-of-items",
-          title: "List of items",
-        },
-        {
-          id: uuidv4(),
-          path: "/checked-names",
-          title: "Checked Names",
-        },
-        {
-          id: uuidv4(),
-          path: "/do-you-like-tacos",
-          title: "Do you like tacos? (radio example)",
-        },
-        {
-          id: uuidv4(),
-          path: "/iterate-an-object",
-          title: "Iterate an object",
-        },
-        {
-          id: uuidv4(),
-          path: "favorite-kind-of-taco",
-          title: "Favorite kind of taco? (v-pre and v-once)",
-        },
-        {
-          id: uuidv4(),
-          path: "backpack-shop-counter",
-          title: "Backpack Shop Counter",
-        },
-        {
-          id: uuidv4(),
-          path: "cross-site-scripting",
-          title: "Cross Site Scripting - XSS Software Attack (v-html)",
-        },
-      ],
+      pages: pagesFixture,
     });
     //#endregion
 
