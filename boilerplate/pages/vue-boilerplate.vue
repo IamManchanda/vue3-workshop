cc5bc4d7affdcdb46acb90727ee5c2257f3dc7ee
Harry Manchanda <harmanmanchanda182@gmail.com>
2020-09-26 21:31:29 +0530

    move Boilerplate code to boilerplate folder

---
+++ boilerplate/pages/vue-boilerplate.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="page-vue-boilerplate"></div>
+</template>
+
+<script>
+//#region Imports
+import { reactive, toRefs } from "vue";
+//#endregion
+
+export default {
+  name: "page-vue-boilerplate",
+  setup() {
+    //#region Reactive References
+    const state = reactive({});
+    //#endregion
+
+    //#region Watchers
+    //#endregion
+
+    //#region Lifecycle hooks
+    //#endregion
+
+    //#region Methods
+    //#endregion
+
+    return {
+      ...toRefs(state),
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.page-vue-boilerplate {
+}
+</style>
