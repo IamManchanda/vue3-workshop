89f460b67983f66dc25798baa2abf58f23a989cc
Harry Manchanda <harmanmanchanda182@gmail.com>
2020-09-26 20:18:58 +0530

    Move app styling to assets => styles folder

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,3 @@
 <template>
   <router-view />
 </template>
-
-<style lang="scss">
-#app {
-  font-family: "HelveticaNeue-Light", "Helvetica Neue Light", "Helvetica Neue",
-    Helvetica, Arial, "Lucida Grande", sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-  padding: 1rem;
-}
-</style>
