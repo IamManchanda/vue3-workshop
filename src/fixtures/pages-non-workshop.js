70585b44b48c71c4724e264dda915307ae5ffcc4
Harry Manchanda <harmanmanchanda182@gmail.com>
2020-09-27 01:06:26 +0530

    content changes

--- src/fixtures/pages-non-workshop.js
+++ src/fixtures/pages-non-workshop.js
@@ -3,12 +3,12 @@ import { v4 as uuidv4 } from "uuid";
 export default [
   {
     id: uuidv4(),
-    path: "/profile-skeleton",
-    title: "Profile Card & Info with Skeleton",
+    path: "/counter",
+    title: "Counter",
   },
   {
     id: uuidv4(),
-    path: "/counter",
-    title: "Counter",
+    path: "/profile-skeleton",
+    title: "Profile Card with Skeleton loading screen",
   },
 ];
