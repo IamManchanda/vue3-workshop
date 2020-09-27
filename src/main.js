3f1e579d9d59d0ab2d332877449ea88cd8cdb706
Harry Manchanda <harmanmanchanda182@gmail.com>
2020-09-27 18:46:59 +0530

    remove vuex as it's not needed

--- src/main.js
+++ src/main.js
@@ -2,11 +2,8 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
-import store from "./store";
 
 import "./assets/styles/index.scss";
 
-createApp(App)
-  .use(store)
-  .use(router)
-  .mount("#app");
+const app = createApp(App).use(router);
+app.mount("#app");
